91594dcbf6a77457bb9aadc9f4f4188fce63f574
Raj Patel <rajpatel@example.com>
2025-04-26 16:48:44 -0400

    Initial commit - Frontend code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to E-commerce Frontend</h1>
+      <p>Product List coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
